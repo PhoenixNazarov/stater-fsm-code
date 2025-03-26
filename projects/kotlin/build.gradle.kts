0cbf3002aec0e9d3afea38e01c4bdadc2fb3ceb9
phoenix <vova24848@gmail.com>
2025-03-26 09:13:32 +0300

    SF-25: add kotlin CI

--- projects/kotlin/build.gradle.kts
+++ projects/kotlin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("jacoco")
 }
 
 group = "org.example"
@@ -17,6 +18,13 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // Запускать после тестов
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
 kotlin {
     jvmToolchain(20)
